4ca2665729ab1b4c88999a20855876c9ae9cbe43
Asad <asadkhakimov05@gmail.com>
2023-08-16 21:59:02 +0600

    add FiltrBook

---
+++ src/components/FiltrBook/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FiltrBook = () => {
+  return (
+    <div id="filtrBook">
+      <div className="container">
+        <div className="filtrBook">filtrBook</div>
+      </div>
+    </div>
+  );
+}
+
+export default FiltrBook
